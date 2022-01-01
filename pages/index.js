4fdde991046d64d937c641beace4a36c88a2e363
AugustBemsen <danireptor7@gmail.com>
2022-01-01 23:38:31 +0100

    implete home screen ui

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,34 @@
 import Head from "next/head";
+import { Icon, Input, InputGroup, InputLeftElement } from "@chakra-ui/react";
+import { AiOutlineSearch } from "react-icons/ai";
+import Logo from "../components/Logo/Logo";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
-    <div>
+    <>
       <Head>
         <title>Jurata</title>
         <meta name="description" content="Jurata Assignment" />
-        <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1>Hello World</h1>
-    </div>
+      <div className={styles.hero}>
+        <div className={styles.heroLogo}>
+          <Logo className={styles.heroLogo} colored />
+        </div>
+        <div className={styles.inputField}>
+          <InputGroup>
+            <InputLeftElement pointerEvents="none">
+              <Icon viewBox="0 0 0 0" as={AiOutlineSearch} />
+            </InputLeftElement>
+            <Input
+              type="text"
+              size="lg"
+              variant="filled"
+              placeholder="Search Jurata"
+            />
+          </InputGroup>
+        </div>
+      </div>
+    </>
   );
 }
